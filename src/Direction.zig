ba14880ac83a2830ef8b2d4e4eb6c0e7997a6928
Shikiyo <shikiyo@archjk>
2023-03-02 16:06:20 -0300

    Add DirectionEnum and helper functions
    Also commented code for z-axis

--- src/Direction.zig
+++ src/Direction.zig
@@ -1,12 +1,61 @@
+const std = @import("std");
+
+// z: i2 = 0,
 y: i2 = 0,
 x: i2 = 0,
 
 const Direction = @This();
 
+// pub const BELOW = Direction{ .z = -1 };
+// pub const ABOVE = Direction{ .z = 1 };
 pub const UP = Direction{ .y = -1 };
 pub const DOWN = Direction{ .y = 1 };
 pub const LEFT = Direction{ .x = -1 };
 pub const RIGHT = Direction{ .x = 1 };
+
+pub const DirectionEnum = enum(u3) {
+    // Above,
+    Up,
+    Right,
+    Down,
+    Left,
+    // Below,
+
+    pub const count = 4; // 6;
+
+    pub fn back(self: DirectionEnum) DirectionEnum {
+        return switch (self) {
+            // .Above => .Below,
+            .Up => .Down,
+            .Right => .Left,
+            .Down => .Up,
+            .Left => .Right,
+            // .Below => .Above,
+        };
+    }
+
+    pub fn toDirection(self: DirectionEnum) Direction {
+        return switch (self) {
+            // .Above => ABOVE,
+            .Up => UP,
+            .Right => RIGHT,
+            .Down => DOWN,
+            .Left => LEFT,
+            // .Below => BELOW,
+        };
+    }
+
+    pub fn inbounds(
+        self: DirectionEnum,
+        comptime Uint: type,
+        y: Uint,
+        x: Uint,
+        h: Uint,
+        w: Uint,
+    ) ?[2]Uint {
+        return self.toDirection().inbounds(Uint, y, x, h, w);
+    }
+};
 
 pub const directions = [_]Direction{ UP, RIGHT, DOWN, LEFT };
 
@@ -29,4 +78,19 @@ pub fn inbounds(
         @intCast(usize, iy),
         @intCast(usize, ix),
     };
+}
+
+pub fn others(
+    self: Direction,
+    buffer: *[DirectionEnum.count]Direction,
+) []Direction {
+    var i = @as(u3, 0);
+    for (std.meta.tags(DirectionEnum)) |de| {
+        const d = de.toDirection();
+        if (!std.meta.eql(self, d)) {
+            buffer[i] = d;
+            i += 1;
+        }
+    }
+    return buffer[0..i];
 }
