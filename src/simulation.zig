Kiyoshi364/yellowstone

commit 11d86fc3df54e1202bc193385ae24db124583f77

Allow Block to be powered by 1 power wire
This allows to extend a signal by 2 tiles per
repeater comparing with using only repeaters.

Also mimics one Redstone behavior.

Example:
* without blocks
  fedcba987654321rfedcba987654321

* before this commit
  fedcba987654321rBfedcba987654321

* after this commit
  fedcba987654321BrBfedcba987654321

(0-f are wires, B is block and r is repeater)

src/simulation.zig
@@ -349,13 +349,12 @@ fn update_block(
                     unreachable;
                 }
             } else if (0 <= this_power and
-                this_power < that_power)
+                0 < that_power)
             {
-                this_power = that_power - 1;
+                this_power = Power.BLOCK_MAX_VALUE;
             }
         }
     }
-    this_power = @min(Power.BLOCK_MAX_VALUE, this_power);
     if (this_power != newstate.power_grid[y][x].power) {
         newstate.power_grid[y][x].power = this_power;
         for (directions) |d| {

commit 1ef6c24f21135df5c34050d1696d7aa42eaf4776

Disallow Block to be powered by another 1 power Block
Fix a bug introduced by commit:
11d86fc Allow Block to be powered by 1 power wire
where non-source Blocks could give power to
adjacent Blocks.

src/simulation.zig
@@ -434,10 +434,21 @@ fn update_block(
                         this_power = power.SOURCE_POWER;
                     }
                 },
-                .empty, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .eleven, .twelve, .thirteen, .fourteen, .fifteen => {
-                    if (0 <= @enumToInt(this_power) and
-                        0 < @enumToInt(that_power))
-                    {
+                .empty => {},
+                .one => {
+                    std.debug.assert(@enumToInt(that_power) == 1);
+                    if (0 <= @enumToInt(this_power)) {
+                        std.debug.assert(that_block == .wire or
+                            that_block == .block);
+                        this_power = if (that_block == .block)
+                            power.BLOCK_OFF_POWER
+                        else
+                            power.BLOCK_ON_POWER;
+                    }
+                },
+                .two, .three, .four, .five, .six, .seven, .eight, .nine, .ten, .eleven, .twelve, .thirteen, .fourteen, .fifteen => {
+                    std.debug.assert(0 < @enumToInt(that_power));
+                    if (0 <= @enumToInt(this_power)) {
                         this_power = power.BLOCK_ON_POWER;
                     }
                 },